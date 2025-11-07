6406eff88aaf34c71f9d066b446dd6a625a1daa1
WellDone88 <kenneth.moren@live.no>
2025-11-07 14:27:40 +0100

    dsfsd

--- view.js
+++ view.js
@@ -3,11 +3,28 @@ const appDiv = document.getElementById('app');
 function updateview(){
     let HTML = /*HTML*/`
     <h2>handlekurv</h2>
-    
-    
-    
-    
-    
+    <ul>
+    `
+
+    for(let i = 0; i < model.handlekurv.length; i++){
+        let minevarer = model.handlekurv[i];
+        HTML += /*HTML*/ `
+        <li style ="display: flex flexdirection: column">
+        <p>Vare: ${minevarer.navn}</p>
+        <p>Pris: ${minevarer.pris} kr</p>
+        <p>antall: ${minevarer.antall} stk</p>
+        </li>
+        
+        `
+    }
+
+
+    HTML += /*HTML*/ `
+    <p>Totalt å betale: ${totalPrice()} kr</p>
+    </ul>
     
     `
+    appDiv.innerHTML = HTML;
 }
+
+updateview()
